b8a81a977839504606af6fecbca7d3d98bf41f64
ilthraim <mobillegos@gmail.com>
2024-02-13 22:03:12 -0500

    Added second layer to pimechdeckkb with bootloader fix

---
+++ keyboards/pimechdeckkb/config.h
@@ -0,0 +1,29 @@
+// Copyright 2023 ilthraim (@ilthraim)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+/*#define ANALOG_JOYSTICK_X_AXIS_PIN F5
+#define ANALOG_JOYSTICK_Y_AXIS_PIN F4
+
+#define ANALOG_JOYSTICK_AXIS_MAX 750
+#define ANALOG_JOYSTICK_AXIS_MIN 200
+#define ANALOG_JOYSTICK_SPEED_REGULATOR 20
+*/
+
